3fdaf8e72708c89722ac277585678d96cdf2c008
Elishaya13 <a.lartillier@gmail.com>
2024-05-21 11:06:47 +0200

    Refactor: Removed unnecessary fragments and reindented code in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className='App'>
       <UsersProvider>
         <RouterProvider router={router} />  
-       </UsersProvider>
+      </UsersProvider>
     </div>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
 import Form from '../components/Form';
 
 const Home = () => {
-  return (
-    <>
+  return (    
       <div className='flex flex-col items-center'>
         <h1 className='text-black py-4 text-lg font-bold'>Create employee </h1>
         <Form />       
-      </div>
-    </>
+      </div>  
   );
 };
 
